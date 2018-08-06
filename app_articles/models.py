c10ff8bf68523c80ddc8afb74fb32eb464a77dbf
eunzz <eunzz123@gmail.com>
2018-08-06 09:51:31 +0900

    Prac_GitKraken

--- app_articles/models.py
+++ app_articles/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Article(models.Model):
   title = models.CharField(max_length=200, verbose_name="제목")
   content = models.TextField(verbose_name="내용")
+  img = models.ImageField(blank=True)
 
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
